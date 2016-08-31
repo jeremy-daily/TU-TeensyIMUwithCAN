54e573e8afefe152d8aef669fdf26c73b0bb0b9f
jeremy-daily <jeremy@synercontechnologies.com>
2016-08-31 18:44:25 -0500

    Used the Adafruit Libraries
    In file \Arduino\libraries\Adafruit_BNO055\Adafruit_BNO055.h on line 33
    change the word “Wire” to “i2c_t3”

--- Vibrations_IntroToSampling/Vibrations_IntroToSampling.ino
+++ Vibrations_IntroToSampling/Vibrations_IntroToSampling.ino
@@ -1,38 +1,47 @@
-/* LED Blink, Teensyduino Tutorial #1
-   http://www.pjrc.com/teensy/tutorial.html
- 
-   This example code is in the public domain.
-*/
+#include <i2c_t3.h> // the I2C library that replaces Wire.h for the Teensy 3.2
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
-// Teensy 2.0 has the LED on pin 11
-// Teensy++ 2.0 has the LED on pin 6
-// Teensy 3.0 has the LED on pin 13
-
-#include  <i2c_t3.h> // the I2C library that replaces Wire.h for the Teensy 3.2
+#include <FlexCAN.h>
+#include <EEPROM.h>
 
 #define PS0Pin      2
 #define resetPin    22
 #define IMUIntPin   6
 #define STPin       17
+#define ledPin      13
 
-#define gyroScaleFactor  0.003814697265625  //Set to 125deg/s / 2^1
-#define gyroOffset      -0.122513335        //Used to zero out the rate gyro when it is still. Uses a longterm average.
+/* This driver reads raw data from the BNO055
 
-#include "BNO055.h"
+   Connections
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
 
+   History
+   =======
+   2015/MAR/03  - First release (KTOWN)
+*/
 
-#define ledPin LED_BUILTIN
-
+/* Set the delay between fresh samples */
 elapsedMillis ledTimer;
 elapsedMillis ledDuration;
 
 boolean ledState = false;
 
-// the setup() method runs once, when the sketch starts
+Adafruit_BNO055 bno = Adafruit_BNO055();
 
-void setup() {
-  // initialize the digital pin as an output.
-  pinMode(ledPin, OUTPUT);
+/**************************************************************************/
+/*
+    Arduino setup function (automatically called at startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+   pinMode(ledPin, OUTPUT);
  
   pinMode(PS0Pin,OUTPUT);
   digitalWrite(PS0Pin,LOW);
@@ -46,114 +55,99 @@ void setup() {
   
   delay(20);
   digitalWrite(resetPin,HIGH);
+  Serial.begin(9600);
+  delay(1000);
   
-      Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
+  Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
 
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
 
-  Serial.begin(115200); //debug console
   delay(1000);
 
-  Serial.println("Starting Bosch BNO055 IMU Sensor... ");
+  /* Display the current temperature */
+  int8_t temp = bno.getTemp();
+  Serial.print("Current Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println("");
 
-  /*
-   System Status:
-   0 System idle,
-  1 System Error,
-  2 Init ializing peripherals
-  3 System Init ializat ion
-  4 Execut ing selftest ,
-  5 Sensor fusion algorithm running,
-  6 System running without fusion algorithm
-   */
-  while (getBNOSystemStatus() < 5 ){
-    
-    BNOwrite(BNO055_OPR_MODE_ADDR, OPERATION_MODE_CONFIG); //Set to configuration Mode
-    delay(20);
-    BNOwrite(BNO055_OPR_MODE_ADDR,OPERATION_MODE_AMG);  
-    delay(200);
-  }
-  Serial.println("Verifying BNO055 Settings...");
-  getBNO055Status();
-  
-//
-//    BNOwrite(BNO055_OPR_MODE_ADDR, OPERATION_MODE_CONFIG); //Set to configuration Mode
-//    delay(20);
-//    BNOwrite(BNO055_OPR_MODE_ADDR, OPERATION_MODE_CONFIG); //Set to configuration Mode
-//    delay(20);
-//    BNOwrite(BNO055_OPR_MODE_ADDR, OPERATION_MODE_CONFIG); //Set to configuration Mode
-//    delay(20);
-//    Serial.println("Done.");
-//
-//    // Set to use external oscillator
-//    Serial.print("Setting up for External oscillator... ");
-//    if (BNOread(BNO055_SYS_CLK_STAT_ADDR) == 0) {
-//      BNOwrite(BNO055_SYS_TRIGGER_ADDR, 0b10000000);
-//      Serial.println("BNO055 set to use external oscillator");
-//    }
-//    else
-//    {
-//      BNOwrite(BNO055_SYS_TRIGGER_ADDR, 0b10000000);
-//      Serial.println("Problem setting BNO055 to use external oscillator.");
-//    }
-//    delay(10);
-//
-//    Serial.print("Setting up Rate Gyro... ");
-//    BNOwrite(BNO055_PAGE_ID_ADDR, 1); // Set to page 1
-//    delay(10);
-//    BNOwrite(GYRO_CONF_0, 0b00101100); // Set gyroscope to 125deg/s at 23Hz (see pg 28 of datasheet)
-//    delay(10);
-//    BNOwrite(GYRO_CONF_1, 0); // Set gyroscope  (see pg 28 of datasheet)
-//    delay(10);
-//
-//
-//    Serial.println("Done.");
-//    Serial.print("Setting up Magnetometer... ");
-//    BNOwrite(MAG_CONF, 0b00011001); //
-//    delay(10);
-//    Serial.println("Done.");
-//
-//    Serial.print("Setting up Accelerometer... ");
-//    BNOwrite(ACC_CONF, 0); //
-//    delay(10);
-//    Serial.println("Done.");
-//
-//    Serial.print("Setting up Units... ");
-//    BNOwrite(BNO055_PAGE_ID_ADDR, 0); // Set to page 0
-//    delay(10);
-//    BNOwrite(BNO055_UNIT_SEL_ADDR, 0b00010000); // Set units to M/s^2 Deg/sec, Deg, Deg F
-//    delay(10);
-//    Serial.println("Done.");
-//
-//    Serial.print("Turning on Sensors... ");
-//    //BNOwrite(BNO055_OPR_MODE_ADDR, OPERATION_MODE_AMG); //
-//    BNOwrite(BNO055_OPR_MODE_ADDR,OPERATION_MODE_NDOF);//
-//    delay(10);
-//    
-//    delay(10);
-//    Serial.println("Done.");
-//
-//    Serial.println("Verifying BNO055 Settings...");
-//    opMode = getBNO055Status();
-//  }
-//  Serial.println("Done.");
+  bno.setExtCrystalUse(true);
 
-
-  
+  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
 }
 
-// the loop() method runs over and over again,
-// as long as the board has power
-
-void loop() {
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+void loop(void)
+{
   if ( ledTimer >= 50){
     ledTimer = 0;
     ledState = !ledState;
     digitalWrite(ledPin,ledState);
-    Serial.print(BNOgetAccelX());
-    Serial.print("\t");
-    Serial.println(BNOgetYawRate());
-  }
-  
- 
-}
 
+    // Possible vector values can be:
+    // - VECTOR_ACCELEROMETER - m/s^2
+    // - VECTOR_MAGNETOMETER  - uT
+    // - VECTOR_GYROSCOPE     - rad/s
+    // - VECTOR_EULER         - degrees
+    // - VECTOR_LINEARACCEL   - m/s^2
+    // - VECTOR_GRAVITY       - m/s^2
+    imu::Vector<3> acceleration = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  
+    /* Display the floating point data */
+    Serial.print("X: ");
+    Serial.print(acceleration.x());
+    Serial.print(" Y: ");
+    Serial.print(acceleration.y());
+    Serial.print(" Z: ");
+    Serial.print(acceleration.z());
+    Serial.print("\t\t");
+    
+    imu::Vector<3> angularVelocity = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+      /* Display the floating point data */
+    Serial.print("X: ");
+    Serial.print(angularVelocity.x());
+    Serial.print(" Y: ");
+    Serial.print(angularVelocity.y());
+    Serial.print(" Z: ");
+    Serial.print(angularVelocity.z());
+    Serial.print("\t\t");
+  
+    /*
+    // Quaternion data
+    imu::Quaternion quat = bno.getQuat();
+    Serial.print("qW: ");
+    Serial.print(quat.w(), 4);
+    Serial.print(" qX: ");
+    Serial.print(quat.y(), 4);
+    Serial.print(" qY: ");
+    Serial.print(quat.x(), 4);
+    Serial.print(" qZ: ");
+    Serial.print(quat.z(), 4);
+    Serial.print("\t\t");
+    */
+  
+    /* Display calibration status for each sensor. */
+    uint8_t system, gyro, accel, mag = 0;
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+    Serial.print("CALIBRATION: Sys=");
+    Serial.print(system, DEC);
+    Serial.print(" Gyro=");
+    Serial.print(gyro, DEC);
+    Serial.print(" Accel=");
+    Serial.print(accel, DEC);
+    Serial.print(" Mag=");
+    Serial.println(mag, DEC);
+
+  }
+}
